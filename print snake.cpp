d88b10d0f5d0db37225461d830d89d9597747652
Ali lashkari <87035757+AliLashkari3375@users.noreply.github.com>
2021-10-26 18:32:26 +0330

    Add files via upload

---
+++ print snake.cpp
@@ -0,0 +1,19 @@
+﻿// print snake.cpp : This file contains the 'main' function. Program execution begins and ends there.
+////Ali lashkri
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cout << "Enter the length of the snake:";
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cout << "*#";
+	}
+	cout << "D";
+}//Ali lashkri
